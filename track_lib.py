dd05a909f9c05e0b49674dd81dfdf265268fce5b
barberj <barber.justin@gmail.com>
2012-03-26 13:35:25 -0400

    fog1465

--- track_lib.py
+++ track_lib.py
@@ -4,9 +4,11 @@ from random import randrange
 try:
     import erp.model as m
     import erp.model.testing as tst
+    from erp.model.lib.history import set_history
 except ImportError:
     import portal.model as m
     import portal.model.testing as tst
+    from portal.model.lib.history import set_history
 
 testing_account = tst.Account.get(352)
 
@@ -84,6 +86,70 @@ def create_irs_track(account_test_ids = [17611,17614,17590]):
 
     return track
 
+def create_track_fog1465():
+    """
+    Create 2 batteries for Fresno School District
+    """
+
+    # get the testing account
+    fresno = m.Account.get(24828).testing_account
+
+    # the spanish tests:
+    """
+    In [9]: for test in fresno.tests:
+        test.id, test.description, test.method.short_name
+       ....:
+       ....:
+    Out[10]: (19707L, u'Spanish Reading Test ILR 2+ to 3+/ALTA 8 to 10', u'Online')
+    Out[10]: (19708L, u'Spanish Writing Test', u'Online')
+    Out[10]: (19709L, u'Spanish Writing Test', u'Paper')
+    Out[10]: (19710L, u'Spanish Listening & Speaking Test', u'Live')
+    """
+
+    reading = tst.AccountTest.get(19707L)
+    live = tst.AccountTest.get(19710L)
+    writing_tests = []
+    writing_tests.append(tst.AccountTest.get(19708L))
+    writing_tests.append(tst.AccountTest.get(19709L))
+
+    # create track
+    tracks = []
+
+    for writing_test in writing_tests:
+
+        # create the track
+        track = tst.Track()
+
+        # describe the track
+        track.description = '%s Battery RWS %s' %\
+                (writing_test.test.languages[0].language, writing_test.method.description.split()[0])
+
+        # append to list so we can return it
+        tracks.append(track)
+
+        # attributes
+        track.account = fresno
+        track.is_global = True
+
+        tests = [reading, writing_test, live]
+        for test in tests:
+            # create our tracktests
+            tracktest = tst.TrackTest()
+            tracktest.account = fresno
+            tracktest.track = track
+            tracktest.test = test
+            tracktest.position = tests.index(test)
+
+    # update our history
+    for track in tracks:
+        for track_test in track.track_tests:
+            set_history(track_test)
+        set_history(track)
+
+    m.meta.Session.commit()
+
+    return tracks
+
 def scheduletest():
     jbarber = tst.CandidateProfile.get(117680)
     test = tst.AccountTest.query.filter_by(archived=False).\
@@ -100,7 +166,6 @@ def scheduletest():
     sct.number = sct.id
 
     return sct
-
 
 def add_track_permissions():
     # admin
